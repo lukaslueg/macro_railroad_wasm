b2c416729bd41297ed5df50ae7415ab33cd467cb
Lukas Lueg <lukas.lueg@gmail.com>
2018-08-13 18:36:00 +0200

    Init

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+#![allow(stable_features)]
+#![feature(proc_macro)]
+#![feature(use_extern_macros)]
+
+#[macro_use]
+extern crate stdweb;
+extern crate railroad;
+extern crate macro_railroad;
+extern crate syn;
+extern crate htmlescape;
+
+use stdweb::js_export;
+
+
+#[js_export]
+fn to_diagram_node(src: &str, hide_internal: bool, foldcommontails: bool, legend: bool) -> String {
+    match to_diagram(src, hide_internal, foldcommontails, legend) {
+        Err(e) => {
+            format!(r#"
+Failed to parse, and I didn't even write an error-handler. Anyway:
+<pre>{:?}</pre>
+"#, e)
+        },
+        Ok((name, diagram)) => {
+            format!(r#"
+<h3>Syntax diagram for macro `{}`</h3>
+<div style="width: {}px; height: auto; max-height: 100%; max-width: 100%">
+{}
+</div>
+"#, name, (&diagram as &railroad::RailroadNode).width(), diagram)
+        }
+    }
+}
+
+fn to_diagram(src: &str, hide_internal: bool, foldcommontails: bool, legend: bool) -> Result<(String, railroad::Diagram<Box<railroad::RailroadNode>>), syn::synom::ParseError> {
+    let macro_rules = macro_railroad::parser::parse(&src)?;
+
+    let mut tree = macro_railroad::lowering::MacroRules::from(macro_rules);
+
+    if hide_internal {
+        tree.remove_internal();
+    }
+
+    if foldcommontails {
+        tree.foldcommontails();
+    }
+
+    let name = tree.name.clone();
+
+    let mut dia = macro_railroad::diagram::into_diagram(tree, legend);
+
+    dia.add_default_css();
+    macro_railroad::diagram::add_default_css(&mut dia);
+
+    Ok((name, dia))
+}
